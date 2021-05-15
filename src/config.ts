d22d5365c9d8188c59bb63ce6f358e56935758a0
NikSchaefer <nikkschaefer@gmail.com>
2021-05-15 10:54:10 -0500

    Blog Homepage

--- src/config.ts
+++ src/config.ts
@@ -33,4 +33,85 @@ const HeaderLinks: HeaderLink[] = [
 	},
 ];
 
-export { HeaderLinks };
+type BlogLink = {
+	link: string;
+	title: string;
+	date?: string;
+	description: string;
+	image: string;
+	min: number;
+};
+
+const BlogLinks: BlogLink[] = [
+	{
+		title: "Why Developers love Golang",
+		link: "/blog/why-developers-love-golang",
+		image: "/blog/codeonlaptop.jpg",
+		description:
+			"Golang(Go) is a programming language developed by Google in 2009.",
+		min: 3,
+		date: "March 2020",
+	},
+	{
+		title: "Why I use Django, and you should too",
+		link: "/blog/why-django-is-great",
+		image: "/blog/setting-up-django.jpg",
+		description:
+			"A quick look at the benefits and reasons why so many developers use Django",
+		min: 2,
+		date: "Feb 2020",
+	},
+	{
+		title: "How to Use a .Dockerfile",
+		link: "/blog/using-a-dockerfile",
+		image: "/blog/docker.png",
+		min: 2,
+		description: "Quickly learn the basics of using a dockerfile",
+		date: "Jan 2020",
+	},
+	{
+		title: "A Beginner's First Machine Learning Model",
+		link: "/blog/beginner-machine-learning-model",
+		image: "/blog/ml.webp",
+		min: 2,
+		description:
+			"learn the basics of a machine learning model in tensorflow",
+		date: "Jan 2020",
+	},
+	{
+		title: "Rest API Authentication in Django",
+		link: "/blog/rest-api-auth-django",
+		image: "/blog/django-rest-auth.jpg",
+		min: 2,
+		date: "Jan 2020",
+		description: "learn to set up a quick user auth server with django",
+	},
+	{
+		title: "Why I use NextJS over Gatsby for Personal Projects",
+		link: "/blog/why-I-choose-nextjs-over-gatsby",
+		image: "/blog/nextjs.jpg",
+		min: 2,
+		description:
+			"Here is a quick breakdown of the differences between NextJS and Gatsby andwhy I chose NextJS",
+		date: "Jan 2020",
+	},
+	{
+		title: "The Basics of Webpack",
+		link: "/blog/webpack-basics",
+		image: "/blog/webpack.jpg",
+		min: 2,
+		description: "Quickly set up and use webpack for production in 2 min",
+		date: "Jan 2020",
+	},
+	{
+		title: "Why you should migrate to Typescript",
+		link: "/blog/why-you-should-use-typescript",
+		image: "/blog/typescript.svg",
+		min: 2,
+		description:
+			"Learn why you should use typescript going forward as well as learn some typescript",
+		date: "Jan 2020",
+	},
+];
+
+export { HeaderLinks, BlogLinks };
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,47 @@
+import Layout from "@components/layout";
+import { BlogLinks } from "config";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledLi = styled.li`
+	text-align: left;
+	margin: 50px 0;
+	a {
+		font-size: 24px;
+		color: #00a2ff;
+	}
+	h3 {
+		margin: 0;
+		color: rgba(0, 0, 0, 0.774);
+		font-weight: 400;
+	}
+	h2 {
+		margin: 10px 0;
+		font-weight: 400;
+		font-size: 20px;
+	}
+`;
+const StyledContainer = styled.ul`
+	width: 90%;
+	max-width: 700px;
+	margin: auto;
+`;
+
 export default function Blog(): JSX.Element {
-	return <div />;
+	return (
+		<Layout>
+			<h1>Blog</h1>
+			<StyledContainer>
+				{BlogLinks.map((data) => (
+					<StyledLi key={data.link}>
+						<Link href={data.link}>
+							<a>{data.title}</a>
+						</Link>
+						<h3>{data.date}</h3>
+						<h2>{data.description}</h2>
+					</StyledLi>
+				))}
+			</StyledContainer>
+		</Layout>
+	);
 }
