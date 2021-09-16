fc921739c0182b288f40e570e9ded70abc23ece6
NikSchaefer <nikkschaefer@gmail.com>
2021-09-15 20:05:50 -0500

    portfolio page

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type Project = {
 	title: string;
 	github: string;
@@ -6,65 +8,95 @@ type Project = {
 	slug: string;
 	text?: string;
 };
+type SortOf = {
+	name: string;
+	include: string[];
+};
+const sortByOptions: SortOf[] = [
+	{
+		name: "ML",
+		include: ["Tensorflow", "tensorflow"],
+	},
+	{
+		name: "Web Dev",
+		include: ["Javascript", "Typescript", "React", "Next.js"],
+	},
+	{
+		name: "Python",
+		include: ["Python", "Tensorflow", "Pandas"],
+	},
+	{
+		name: "Golang",
+		include: ["Go", "Golang", "go", "golang"],
+	},
+];
+
+function calculateType(tech: string[]): string {
+	const out: string[] = [];
+
+	for (const option of sortByOptions) {
+		if (tech.some((r) => option.include.includes(r))) {
+			out.push(option.name);
+		}
+	}
+
+	return out.join("/");
+}
 
 // eslint-disable-next-line import/no-default-export
 export default function Card(props: { project: Project }): JSX.Element {
 	return (
-		<div className="w-full max-w-sm px-4 py-3 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-			<div className="flex items-center justify-between">
-				<span className="text-sm font-light text-gray-800 dark:text-gray-400">
-					{props.project.tech[0]}
-				</span>
-				<span className="px-3 py-1 text-xs text-indigo-800 uppercase bg-indigo-200 rounded-full dark:bg-indigo-300 dark:text-indigo-900">
-					{props.project.tech[0]}
-				</span>
-			</div>
-			<div>
-				<h1 className="mt-2 text-lg font-semibold text-gray-800 dark:text-white">
-					{props.project.title}
-				</h1>
-				<p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
-					{props.project.text}
-				</p>
-			</div>
-
-			<div>
-				<div className="flex items-center mt-2 text-gray-700 dark:text-gray-200">
-					<span>Visit on:</span>
-					<a className="mx-2 text-blue-600 cursor-pointer dark:text-blue-400 hover:underline">
-						edx.org
-					</a>
-					<span>or</span>
-					<a className="mx-2 text-blue-600 cursor-pointer dark:text-blue-400 hover:underline">
-						classNamecentral.com
-					</a>
+		<Link href={`/portfolio/${props.project.slug}`}>
+			<a className="w-full max-w-sm px-4 py-3 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 dark:hover:bg-gray-700 hover:bg-gray-100">
+				<div className="flex items-center justify-between">
+					<span className="text-sm font-light text-gray-800 dark:text-gray-400">
+						{props.project.tech[0]}
+					</span>
+					<span className="px-3 py-1 text-xs text-indigo-800 uppercase bg-indigo-200 rounded-full dark:bg-indigo-300 dark:text-indigo-900">
+						{calculateType(props.project.tech)}
+					</span>
+				</div>
+				<div>
+					<h1 className="mt-2 text-lg font-semibold text-gray-800 dark:text-white">
+						{props.project.title}
+					</h1>
+					<p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
+						{props.project.text}
+					</p>
 				</div>
 
-				<div className="flex items-center justify-center mt-4">
-					<a className="mr-2 text-gray-800 cursor-pointer dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300">
-						<svg
-							className="w-5 h-5 fill-current"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 512 512"
+				<div>
+					<div className="flex items-center justify-center mt-4">
+						<a
+							href={props.project.github}
+							className="mr-2 text-gray-800 cursor-pointer dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300"
 						>
-							<path d="M496,109.5a201.8,201.8,0,0,1-56.55,15.3,97.51,97.51,0,0,0,43.33-53.6,197.74,197.74,0,0,1-62.56,23.5A99.14,99.14,0,0,0,348.31,64c-54.42,0-98.46,43.4-98.46,96.9a93.21,93.21,0,0,0,2.54,22.1,280.7,280.7,0,0,1-203-101.3A95.69,95.69,0,0,0,36,130.4C36,164,53.53,193.7,80,211.1A97.5,97.5,0,0,1,35.22,199v1.2c0,47,34,86.1,79,95a100.76,100.76,0,0,1-25.94,3.4,94.38,94.38,0,0,1-18.51-1.8c12.51,38.5,48.92,66.5,92.05,67.3A199.59,199.59,0,0,1,39.5,405.6,203,203,0,0,1,16,404.2,278.68,278.68,0,0,0,166.74,448c181.36,0,280.44-147.7,280.44-275.8,0-4.2-.11-8.4-.31-12.5A198.48,198.48,0,0,0,496,109.5Z" />
-						</svg>
-					</a>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="w-5 h-5 fill-current"
+								viewBox="0 0 24 24"
+							>
+								<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+							</svg>
+						</a>
 
-					<a className="mr-2 text-gray-800 cursor-pointer dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300">
-						<svg
-							className="w-5 h-5 fill-current"
-							viewBox="0 0 24 24"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path d="M14.8283 12L16.2426 13.4142L19.071 10.5858C20.6331 9.02365 20.6331 6.49099 19.071 4.9289C17.5089 3.3668 14.9762 3.3668 13.4141 4.9289L10.5857 7.75732L11.9999 9.17154L14.8283 6.34311C15.6094 5.56206 16.8757 5.56206 17.6568 6.34311C18.4378 7.12416 18.4378 8.39049 17.6568 9.17154L14.8283 12Z" />
-							<path d="M12 14.8285L13.4142 16.2427L10.5858 19.0711C9.02365 20.6332 6.49099 20.6332 4.9289 19.0711C3.3668 17.509 3.3668 14.9764 4.9289 13.4143L7.75732 10.5858L9.17154 12L6.34311 14.8285C5.56206 15.6095 5.56206 16.8758 6.34311 17.6569C7.12416 18.4379 8.39049 18.4379 9.17154 17.6569L12 14.8285Z" />
-							<path d="M14.8284 10.5857C15.2189 10.1952 15.2189 9.56199 14.8284 9.17147C14.4379 8.78094 13.8047 8.78094 13.4142 9.17147L9.17154 13.4141C8.78101 13.8046 8.78101 14.4378 9.17154 14.8283C9.56206 15.2188 10.1952 15.2188 10.5857 14.8283L14.8284 10.5857Z" />
-						</svg>
-					</a>
+						<Link href={`/portfolio/${props.project.slug}`}>
+							<a className="mr-2 text-gray-800 cursor-pointer dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300">
+								<svg
+									className="w-5 h-5 fill-current"
+									viewBox="0 0 24 24"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path d="M14.8283 12L16.2426 13.4142L19.071 10.5858C20.6331 9.02365 20.6331 6.49099 19.071 4.9289C17.5089 3.3668 14.9762 3.3668 13.4141 4.9289L10.5857 7.75732L11.9999 9.17154L14.8283 6.34311C15.6094 5.56206 16.8757 5.56206 17.6568 6.34311C18.4378 7.12416 18.4378 8.39049 17.6568 9.17154L14.8283 12Z" />
+									<path d="M12 14.8285L13.4142 16.2427L10.5858 19.0711C9.02365 20.6332 6.49099 20.6332 4.9289 19.0711C3.3668 17.509 3.3668 14.9764 4.9289 13.4143L7.75732 10.5858L9.17154 12L6.34311 14.8285C5.56206 15.6095 5.56206 16.8758 6.34311 17.6569C7.12416 18.4379 8.39049 18.4379 9.17154 17.6569L12 14.8285Z" />
+									<path d="M14.8284 10.5857C15.2189 10.1952 15.2189 9.56199 14.8284 9.17147C14.4379 8.78094 13.8047 8.78094 13.4142 9.17147L9.17154 13.4141C8.78101 13.8046 8.78101 14.4378 9.17154 14.8283C9.56206 15.2188 10.1952 15.2188 10.5857 14.8283L14.8284 10.5857Z" />
+								</svg>
+							</a>
+						</Link>
+					</div>
 				</div>
-			</div>
-		</div>
+			</a>
+		</Link>
 	);
 }
--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,21 +1,10 @@
-import Card from '@components/card'
+import Card from "@components/card";
+import { H1, Section } from "@components/design";
 import Layout from "@components/layout";
-import Meta, { MetaType } from "@components/meta";
-import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import styled from "styled-components";
 
-import json from "../../../content/featured.json";
 import projects from "../../../content/projects.json";
-
-const META: MetaType = {
-	title: "Portfolio | Nik Schaefer",
-	lang: "en-us",
-	description: " My Projects and things I've built",
-	image: "/logo.png",
-	url: "https://nikschaefer.tech/portfolio",
-};
 
 type Project = {
 	title: string;
@@ -32,120 +21,15 @@ type SortOf = {
 	content: Project[];
 };
 
-export const StyledCard = styled.a`
-	display: flex;
-	text-align: left;
-	flex-direction: column;
-	width: 240px;
-	height: 380px;
-	border: solid rgba(0, 0, 0, 0.219) 1px;
-	margin: 10px;
-	.text {
-		background-color: var(--secondary);
-		height: 55%;
-		box-sizing: border-box;
-		padding: 10px;
-		h2 {
-			font-weight: 500;
-		}
-		p {
-			font-family: "Mukta", sans-serif;
-			color: var(--about-text);
-			margin: unset;
-			font-size: unset;
-		}
-	}
-
-	.image {
-		background-color: var(--primary);
-		height: 45%;
-		justify-content: center;
-		align-items: center;
-		display: flex;
-		border-bottom: solid rgba(0, 0, 0, 0.219) 1px;
-	}
-	:hover {
-		box-shadow: 0 5px 5px rgba(68, 68, 68, 0.205);
-		cursor: pointer;
-	}
-`;
-export const Container = styled.section`
-	display: flex;
-	flex-wrap: wrap;
-	width: 90%;
-	justify-content: center;
-	max-width: 1300px;
-	margin: auto;
-	flex: 1;
-`;
-export const size = 80;
-
 const ProjectContainer = styled.div`
 	display: flex;
 	flex-flow: wrap row;
 	justify-content: center;
-	width: 90%;
-	max-width: 1100px;
+	width: 95%;
+	max-width: 1200px;
 	margin: auto;
 	row-gap: 1rem;
 	column-gap: 1rem;
-`;
-const Project = styled.a`
-	width: 20%;
-	min-width: 300px;
-	background-color: var(--secondary);
-	box-sizing: border-box;
-	padding: 10px;
-	position: relative;
-	border-radius: 10px;
-	:hover {
-		background-color: var(--grey);
-		cursor: pointer;
-		a {
-			transform: rotate(20deg);
-		}
-	}
-	h4 {
-		font-weight: 500;
-	}
-	a {
-		position: absolute;
-		top: 5px;
-		right: 5px;
-		padding: 5px;
-		transition: transform 0.3s ease;
-		:hover {
-			transform: rotate(40deg);
-		}
-	}
-	i {
-		height: 12px;
-		width: 12px;
-		display: inline-block;
-		background-color: #ff5f57;
-		border-radius: 50%;
-		position: absolute;
-		top: 6px;
-		left: 10px;
-		::before,
-		::after {
-			content: " ";
-			top: 0;
-			position: absolute;
-			height: 12px;
-			width: 12px;
-			border-radius: 50%;
-			display: block;
-		}
-		:before {
-			left: 16px;
-			background-color: #ffbd2e;
-		}
-		::after {
-			left: 32px;
-			background-color: #28ca41;
-		}
-	}
 `;
 const StyledList = styled.ul`
 	display: flex;
@@ -167,9 +51,6 @@ const StyledList = styled.ul`
 	.active {
 		border-color: #00a2ff;
 	}
-`;
-const StyledSpan = styled.span`
-	font-size: 15px;
 `;
 const sortByOptions: SortOf[] = [
 	{ name: "All", include: [], content: projects },
@@ -208,34 +89,9 @@ export default function Portfolio(): JSX.Element {
 	const [sortBy, setSortBy] = useState("All");
 	return (
 		<Layout>
-			<Meta META={META} />
-			<h1>Portfolio</h1>
-			<Container>
-				{json.map((data) => (
-					<Link
-						href={`/portfolio/${String(data.slug)}`}
-						key={data.github}
-						passHref
-					>
-						<StyledCard>
-							<div className="image">
-								<Image
-									height={size}
-									width={size}
-									alt={data.title}
-									src={`/featured/${String(data.svg)}`}
-								/>
-							</div>
-							<div className="text">
-								<h2>{data.title}</h2>
-								<p>{data.short}</p>
-							</div>
-						</StyledCard>
-					</Link>
-				))}
-			</Container>
-			<section>
-				<h2>All Projects</h2>
+			<Section class="text-center">
+				<H1>Portfolio</H1>
+				<p>Online collection of my major projects</p>
 				<StyledList>
 					{sortByOptions.map((value) => (
 						<button
@@ -248,11 +104,12 @@ export default function Portfolio(): JSX.Element {
 							type="button"
 							aria-label={`sort by ${value.name}`}
 						>
-							{value.name}{" "}
-							<StyledSpan>{value.content.length}</StyledSpan>
+							{value.name} <span>{value.content.length}</span>
 						</button>
 					))}
 				</StyledList>
+			</Section>
+			<section>
 				<ProjectContainer>
 					{projectData.map((data) => (
 						<Card project={data} key={data.github} />
