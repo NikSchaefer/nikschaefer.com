f2703be3a081e45b264abacc29c45dc88de986a0
NikSchaefer <nikkschaefer@gmail.com>
2021-09-19 18:48:21 -0500

    fix: tailwind into global css file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
