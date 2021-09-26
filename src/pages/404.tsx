8505733fe50b3e492a9192898516a0a949f99c21
NikSchaefer <nikkschaefer@gmail.com>
2021-09-25 21:07:42 -0500

    fix: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,22 @@
-import Layout from "@components/layout";
-
 export default function FourOhFour(): JSX.Element {
 	return (
-		<Layout>
-			<div className="bg-indigo-900 relative overflow-hidden h-screen">
-				<img
-					src="/astro.svg"
-					className="absolute h-full w-full object-cover"
-					alt='404 astronaut'
-				/>
-				<div className="inset-0 bg-black opacity-25 absolute" />
-				<div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
-					<div className="w-full font-mono flex flex-col items-center relative z-10">
-						<h1 className="font-extrabold text-5xl text-center text-white leading-tight mt-4">
-							You&#x27;re alone here
-						</h1>
-						<p className="font-extrabold text-8xl my-44 text-white animate-bounce">
-							404
-						</p>
-					</div>
+		<div className="bg-indigo-900 relative overflow-hidden h-screen">
+			<img
+				src="/astro.svg"
+				className="absolute h-full w-full object-cover"
+				alt="404 astronaut"
+			/>
+			<div className="inset-0 bg-black opacity-25 absolute" />
+			<div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+				<div className="w-full font-mono flex flex-col items-center relative z-10">
+					<h1 className="font-extrabold text-5xl text-center text-white leading-tight mt-4">
+						You&#x27;re alone here
+					</h1>
+					<p className="font-extrabold text-8xl my-44 text-white animate-bounce">
+						404
+					</p>
 				</div>
 			</div>
-		</Layout>
+		</div>
 	);
 }
