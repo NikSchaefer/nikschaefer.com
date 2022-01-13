f9696e36ff4e01f73f9257c95688ff9f5d4d4c47
NikSchaefer <59715513+NikSchaefer@users.noreply.github.com>
2022-01-13 10:25:33 -0600

    feat: clean

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ module.exports = {
 				},
 			},
 			transitionTimingFunction: {
-				'expo': 'cubic-bezier(0.645, 0.045, 0.355, 1);'
+				expo: "cubic-bezier(0.645, 0.045, 0.355, 1);",
 			},
 			colors: {
 				coal: {
