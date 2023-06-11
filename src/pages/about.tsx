13f40bf6997e19595bd92d410a5704f45e075aca
NikSchaefer <59715513+NikSchaefer@users.noreply.github.com>
2023-06-10 19:19:55 -0500

    feat: about page update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,6 +6,8 @@ import jobs from "../../content/jobs.json";
 import studies from "../../content/studies.json";
 import clsx from "clsx";
 import Node from "@components/node";
+import { socials } from "config";
+import { ArrowUpRight, Github, Linkedin, Mail } from "lucide-react";
 
 export default function About(): JSX.Element {
 	return (
@@ -24,13 +26,13 @@ export default function About(): JSX.Element {
 					I'm Nik.
 				</motion.h1>
 			</section>
-			<section className="grid grid-cols-10 pt-[5em] container mx-auto">
+			<section className="flex flex-col items-center lg:grid lg:grid-cols-10 pt-[5em] container mx-auto">
 				<motion.img
 					viewport={{ once: true }}
 					initial={{ y: -10, opacity: 0 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.2, delay: 0.3 }}
-					className="col-span-4"
+					className="col-span-4 w-[80%] mx-auto lg:w-full"
 					src="/developer.svg"
 				/>
 				<div className="col-span-1" />
@@ -39,9 +41,9 @@ export default function About(): JSX.Element {
 					initial={{ y: -10, opacity: 0 }}
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 0.2, delay: 0.5 }}
-					className="flex flex-col col-span-5"
+					className="w-[90%] mx-auto lg:w-full flex flex-col col-span-5"
 				>
-					<span className="mb-2">--- about me in O(1) ---</span>
+					<span className="my-2">--- about me in O(1) ---</span>
 					<h1 className="font-semibold text-effect mb-10 ">
 						I'm a Software Engineer with a passion for building
 					</h1>
@@ -56,6 +58,50 @@ export default function About(): JSX.Element {
 						nonprofit, and balancing my studies as a dual enrollment
 						student.
 					</p>
+					<div className="pt-[2em] flex-col sm:flex-row flex gap-2 h6 font-semibold">
+						<motion.a
+							viewport={{ once: true }}
+							initial={{ y: -10, opacity: 0 }}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{ duration: 0.2, delay: 0.7 }}
+							href={socials.github.link}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="flex gap-4 items-center rounded border px-3 py-4 w-fit border-secondary-400 hover:bg-secondary-600"
+						>
+							<Github />
+							GitHub
+							<ArrowUpRight className="ml-1" />
+						</motion.a>
+						<motion.a
+							viewport={{ once: true }}
+							initial={{ y: -10, opacity: 0 }}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{ duration: 0.2, delay: 0.8 }}
+							href={socials.linkedin.link}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="flex gap-4 items-center rounded border px-3 py-4 w-fit border-secondary-400 hover:bg-secondary-600"
+						>
+							<Linkedin />
+							LinkedIn
+							<ArrowUpRight className="ml-1" />
+						</motion.a>
+						<motion.a
+							viewport={{ once: true }}
+							initial={{ y: -10, opacity: 0 }}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{ duration: 0.2, delay: 0.9 }}
+							href={socials.email.link}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="flex gap-4 items-center rounded border px-3 py-4 w-fit border-secondary-400 hover:bg-secondary-600"
+						>
+							<Mail />
+							Email
+							<ArrowUpRight className="ml-1" />
+						</motion.a>
+					</div>
 				</motion.div>
 			</section>
 			<motion.section
